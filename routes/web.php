00b3e9d28c936be1fc1f7ebb0e3bb2017e24293f
kw1c-JovanovicDanijel <danijel.jovanovic@edu-kw1c.nl>
2025-09-23 14:09:45 +0200

    login page

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
-
 Route::get('/tinker', function () {
     dd();
 });
