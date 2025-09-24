dea049d3907f4f159923e338bdf4cced1c2695a3
kw1c-JovanovicDanijel <danijel.jovanovic@edu-kw1c.nl>
2025-09-24 11:56:32 +0200

    routes made better

--- routes/web.php
+++ routes/web.php
@@ -12,38 +12,13 @@ Route::get('/login', [LoginController::class, 'create'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login');
 Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 
-Route::get('/test', function () {
-    return view('components.test');
-})->name('test');
-
-Route::get('/orders', function () {
-    return view('orders');
-})->name('orders');
-
-Route::get('/companyorders', function () {
-    return view('companyorders');
-})->name('companyorders');
-
-Route::get('/invoices', function () {
-    return view('invoices');
-})->name('invoices');
-
-Route::get('/suppliers', function () {
-    return view('suppliers');
-})->name('suppliers');
-
-Route::get('/customers', function () {
-    return view('customers');
-})->name('customers');
-
-Route::get('/products', function () {
-    return view('products');
-})->name('products');
-
-Route::view('/dashboard','dashboard')->name('dashboard');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::view('/orders', 'orders')->name('orders');
+Route::view('/companyorders', 'companyorders')->name('companyorders');
+Route::view('/invoices', 'invoices')->name('invoices');
+Route::view('/suppliers', 'suppliers')->name('suppliers');
+Route::view('/customers', 'customers')->name('customers');
+Route::view('/products', 'products')->name('products');
+Route::view('/dashboard', 'dashboard')->name('dashboard');
 
 Route::get('/tinker', function () {
     dd();
