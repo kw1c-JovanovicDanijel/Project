aacf2b1f44215e83538f3d7455a08afe5dde6056
Jasper <jasper.vandenheuij@edu-kw1c.nl>
2025-09-24 09:56:27 +0200

    changed web.php

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,16 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // TODO: replace first home with dashboard
-    return Auth::check() ? view('home') : view('home');
+    return Auth::check() ? to_route('dashboard') : view('home');
 })->name('home');
 
 Route::get('/login', [LoginController::class, 'create'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login');
 Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 
-Route::get('/test', function () {
-    return view('components.test');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->name('dashboard');
 
 Route::get('/tinker', function () {
     dd();
