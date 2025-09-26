6c5612b189cdf4c0588b87a0f37cecf84c36ebc9
Jasper <jasper.vandenheuij@edu-kw1c.nl>
2025-09-26 11:06:21 +0200

    cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,7 @@ namespace Database\Seeders;
 
 use App\Enums\UserRoles;
 use App\Models\Address;
-use App\Models\Customer;
-use App\Models\Supplier;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -54,11 +51,5 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Address::factory(100)->create();
-        //        Customer::factory(100)
-        //            ->has(Address::factory(rand(1, 3)))
-        //            ->create();
-        //        Supplier::factory(100)
-        //            ->has(Address::factory(rand(1, 3)))
-        //            ->create();
     }
 }
