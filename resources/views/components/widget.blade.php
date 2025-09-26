5783fa9ff96bb55da4953306fd9eb4e4638f7f4e
kw1c-JovanovicDanijel <danijel.jovanovic@edu-kw1c.nl>
2025-09-26 08:42:34 +0200

    verder gewerkt aan dahboard

--- resources/views/components/widget.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['title', 'data'])
-
-<div class="bg-white p-6 rounded-xl shadow hover:shadow-lg transition">
-    <h3 class="text-gray-600 text-sm">{{ $title }}</h3>
-    <p class="text-2xl font-bold text-orange-500">{{ $data }}</p>
-</div>
