47b414d33fa25d86a2109ba6704406c7660f43ff
kw1c-JovanovicDanijel <danijel.jovanovic@edu-kw1c.nl>
2025-10-22 12:19:07 +0200

    index datums verwijderd

--- resources/views/company_orders/index.blade.php
+++ resources/views/company_orders/index.blade.php
@@ -1,7 +1,7 @@
 <?php
 $orders;
 $name = 'Bestellingen (bedrijf) overzicht';
-$columns = ['referentie nummer', 'orderdatum', 'status', 'gemaakt op', 'geupdate op', 'aantal producten'];
+$columns = ['referentie nummer', 'orderdatum', 'status','aantal producten'];
 $route = 'company-orders';
 ?>
 <x-layout>
